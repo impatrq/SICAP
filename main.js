2687a3f5d35261c98858c83c30a9e59472dea5d5
Tiziano patella <tizianopatella40@gmail.com>
2025-05-21 15:11:50 -0300

    pagina web S.I.C.A.P

---
+++ main.js
@@ -0,0 +1,6 @@
+const botonfacul= document.querySelector("botonfacul");
+const cerrarmodal= document.querySelector("cerrarmodal");
+const modal= document.querySelector("#modal");
+botonfacul.addEventListener("click",()=>{
+    modal.showModal();
+})
