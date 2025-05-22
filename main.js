e5bf817a7c1ef7877b6387a75d040dd24bf965df
Tiziano patella <tizianopatella40@gmail.com>
2025-05-22 16:30:56 -0300

    falle clase de medina

--- main.js
+++
@@ -1,6 +0,0 @@
-const botonfacul= document.querySelector("botonfacul");
-const cerrarmodal= document.querySelector("cerrarmodal");
-const modal= document.querySelector("#modal");
-botonfacul.addEventListener("click",()=>{
-    modal.showModal();
-})
