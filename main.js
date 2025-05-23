4baac26b6998279efce5d1948739ea22cd2fddc7
Tiziano patella <tizianopatella40@gmail.com>
2025-05-23 14:37:50 -0300

    Pagina web- ventanas modales en funcionamineto correcto

---
+++ main.js
@@ -0,0 +1,6 @@
+const botonFacul = document.querySelector("#boton-facul");
+const cerrarModal = document.querySelector("#cerrar-modal");
+const modal = document.querySelector("#modal"); 
+botonFacul.addEventListener("click", ()=>{
+    modal.showModal()
+})
