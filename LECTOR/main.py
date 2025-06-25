a7731178ed75b8d7418dd92fdad43ebc0e0d97c1
facuspagnoletta <101607658+facuspagnoletta@users.noreply.github.com>
2025-06-25 11:30:18 -0300

    COOLDOWN
    Agrego un cooldown, para que cada vez que pase una etiqueta, pase un cierto tiempo hasta que la puedas pasar devuelta.

--- LECTOR/main.py
+++ LECTOR/main.py
@@ -3,14 +3,27 @@ from Reader import RFIDReader
 
 lector = RFIDReader(tx=17, rx=16)
 
+Cooldown = 5 
+lastag = None
+time_lastag = 0
+
+print("Esperando tags...")
+
 while True:
+    
     tag = lector.read_tag()
 
     if tag:
-        print("Tag detectado:", tag)
+        tiempo_actual = time.time()
+        
+        if tag != lastag or (tiempo_actual - time_lastag) > Cooldown:
+            
 
-    else:
-            print("Tag no reconocido")
+            print("Tag leído", {tag})
+            
+            # Actualizo las variables de control
+            lastag = tag
+            time_lastag = tiempo_actual
+    
 
-    time.sleep(1)
-
+    time.sleep(3)
