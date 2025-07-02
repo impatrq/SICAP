caf003f39eebee50d37b88a74944dff18741a754
Tiziano patella <tizianopatella40@gmail.com>
2025-07-02 14:43:54 -0300

    web-app "sicap" eliminada
    al comienzo del desarrollo de la webapp cree una aplicación llamada sicap la cual no termine usando al final por lo que la elimino para evitar confusiones y mejorar la organización de los archivos.

--- Web-app/sicap/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.sicap.app',
-  appName: 'sicap',
-  webDir: 'www'
-};
-
-export default config;
