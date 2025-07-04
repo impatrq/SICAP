fdf458986bdca738aba5ab595b3d72c3e3425cf2
Tiziano patella <tizianopatella40@gmail.com>
2025-07-04 11:07:30 -0300

    Web app x DJango para sección  de login conectada exitosamente
    Logre conectar la web app  con el servidor creado por lauty, cree una app en django para almacenar todos los datos de login. Esta  es la primera versión del código (debe seguir siendo refinado)

--- Servidor-sicap_backend/SICAP_Backend/sicap_backend/settings.py
+++ Servidor-sicap_backend/SICAP_Backend/sicap_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registros',
+    'usuarios_login',
 ]
 
 MIDDLEWARE = [
--- Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
+++ Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('registros.urls')),
+    path('api/recibir/', include('registros.urls')),  # o como tengas configurado
+    path('', lambda request: HttpResponse("¡Bienvenido a la página principal!")),  # Ruta principal
 ]
