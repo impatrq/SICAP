cbccfde67bd13dba2b4bd3848a1dd24947e8d283
Tiziano patella <tizianopatella40@gmail.com>
2025-07-04 16:11:36 -0300

    Envío de datos de login guardados exitosamente en el Django
    La anterior actualización había sido de cuando logre levantar el servidor con la web-app. Lo que logre hacer ahora fue que pueda completar un formulario en una pagina creada en Django y que me guarde esos datos en el servidor. El siguiente paso es remplazar ese mismo formulario que me da Django por el que tengo en la web-app para que me envíe los datos desde ese lugar.

--- Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
+++ Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/recibir/', include('registros.urls')),  # o como tengas configurado
-    path('', lambda request: HttpResponse("¡Bienvenido a la página principal!")),  # Ruta principal
+    path ('usuarios_login/v1/', include('usuarios_login.urls'))
 ]
