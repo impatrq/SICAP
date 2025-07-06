ee068cc2f61d0404279ca1c9983d54f621562451
Tiziano patella <tizianopatella40@gmail.com>
2025-07-06 18:01:05 -0300

    Fetch aplicado al codigo de la web-app
    Fetch es el encargado de que el envió de datos de usuario se hagan desde el html de la aplicación y no desde el formulario del server de Django.

--- Servidor-sicap_backend/SICAP_Backend/usuarios_login/models.py
+++ Servidor-sicap_backend/SICAP_Backend/usuarios_login/models.py
@@ -4,6 +4,4 @@ from django.db import models
 
 class Programmer(models.Model):
     fullname = models.CharField(max_length=100)
-    nickname = models.CharField(max_length=50,)
-    age = models.PositiveSmallIntegerField()
-    is_active = models.BooleanField(default=True)
+    
