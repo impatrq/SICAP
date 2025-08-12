92dc194fc58aa15e646ad9d8fbf9b52fee86905f
facuspagnoletta <101607658+facuspagnoletta@users.noreply.github.com>
2025-08-12 17:17:09 -0300

    .

--- Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
+++ Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    
     path('api/v1/', include('usuarios_login.urls')),
-    
-    
-    path('api/v1/registros/', include('registros.urls')),
+
+    path('admin/', admin.site.urls),
+
 ]
