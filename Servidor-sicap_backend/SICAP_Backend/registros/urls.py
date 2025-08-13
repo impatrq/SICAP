48849f7219fd513aa32a346bf6a219851bf1e80c
kicklauti <101152011+kicklauti@users.noreply.github.com>
2025-08-13 16:53:00 -0300

    feat: login por API + login HTML y dashboard con registros; CORS/CSRF y limpieza de URLs

--- Servidor-sicap_backend/SICAP_Backend/registros/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import recibir_tag
-
-urlpatterns = [
-    
-    path('recibir/', recibir_tag),
-]
--- Servidor-sicap_backend/SICAP_Backend/usuarios_login/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsuariosLoginConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usuarios_login'
