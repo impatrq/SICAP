44ddfdbf7ae94efd0f817dcdaac0ed19b1b9efb0
Tiziano patella <tizianopatella40@gmail.com>
2025-08-20 16:39:40 -0300

    desactive el standalone xq no funcionaba la app

--- Web-app/web-app/src/app/home/home.page.ts
+++ Web-app/web-app/src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  standalone: false
 })
 export class HomePage {
   botones: string[] = ['Opción 1', 'Opción 2'];
