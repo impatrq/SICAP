2936938f731c584870ecac850ff68522f676b9af
facuspagnoletta <101607658+facuspagnoletta@users.noreply.github.com>
2025-08-26 08:37:33 -0300

    Url Menu BÁSICA

--- Web-app/web-app/src/app/menu/menu.module.ts
+++ Web-app/web-app/src/app/menu/menu.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+// 👇 1. IMPORTAMOS EL MÓDULO DE IONIC
 import { IonicModule } from '@ionic/angular';
 
 import { MenuPageRoutingModule } from './menu-routing.module';
-
 import { MenuPage } from './menu.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule,
-    MenuPageRoutingModule
+    MenuPageRoutingModule,
+    IonicModule 
   ],
   declarations: [MenuPage]
 })
--- Web-app/web-app/src/app/menu/menu.page.ts
+++ Web-app/web-app/src/app/menu/menu.page.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
-  standalone: false,
+  standalone: false, 
 })
 export class MenuPage implements OnInit {
+  
+  panoles: any[] = [];
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+    
+    this.panoles = [
+      { id: 1, nombre: 'Pañol de Mecánica' },
+      { id: 2, nombre: 'Pañol de Electricidad' },
+      { id: 3, nombre: 'Pañol Principal' },
+    ];
   }
 
+  
+  seleccionarPanol(panol: any) {
+    console.log('Pañol seleccionado:', panol);
+    
+    this.navCtrl.navigateForward('/home');
+  }
 }
