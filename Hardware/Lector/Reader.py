7a002180664746f2ce70cfae19461b2a31a67771
Tiziano patella <tizianopatella40@gmail.com>
2025-09-09 14:48:06 -0300

    Corrección de código de ESP funcional
    A la fecha de hoy tras todas las modificaciones que hicimos del código del ESP32, este es que anda. Lo único que le falta a este código es verificar el problema de que a pesar de que detecta los tags y nos dice que los envían al servidor, estos realmente no aparecen como nuevos tags en el servidor de Django

--- Hardware/Lector/Reader.py
+++ Hardware/Lector/Reader.py
@@ -1,10 +1,11 @@
 from machine import UART
 import time
 
-# Este es el largo de bits de un tag.
+# Este es el largo de bytes de un tag
 id_bytes = 8
 
 class RFIDReader:
+    
     def __init__(self, tx=17, rx=16, baudrate=9600):
         self.uart = UART(2, baudrate=baudrate, tx=tx, rx=rx)
 
@@ -15,7 +16,5 @@ class RFIDReader:
             
             # Solo procesamos si recibimos la cantidad exacta de bytes esperada.
             if raw_data and len(raw_data) == id_bytes:
-                
                 return raw_data.hex().upper()
-        
-        return None
+            return None
