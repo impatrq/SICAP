0773c2f5ddd3d6737863072b651f39d61ffaffb7
facuspagnoletta <facu.spagnoletta@gmail.com>
2025-09-17 14:21:39 -0300

    Logro de comunicación servidor-backend -> antenas

--- Servidor-sicap_backend/SICAP_Backend/registros/views.py
+++ Servidor-sicap_backend/SICAP_Backend/registros/views.py
@@ -5,15 +5,17 @@ from .models import RegistroTag
 
 @csrf_exempt
 def recibir_tag(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            tag = data.get('tag')
-            if tag:
-                RegistroTag.objects.create(tag=tag)
-                return JsonResponse({'status': 'ok'})
-            else:
-                return JsonResponse({'error': 'tag faltante'}, status=400)
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
-    return JsonResponse({'error': 'método no permitido'}, status=405)
+	if request.method == 'POST':
+		try:
+			data = json.loads(request.body)
+			tag = data.get('tag')
+			print(tag)
+			if tag:
+				RegistroTag.objects.create(tag=tag)
+				return JsonResponse({'status': 'ok'})
+			else:
+				return JsonResponse({'error': 'tag faltante'}, status=400)
+		except Exception as e:
+			return JsonResponse({'error': str(e)}, status=500)
+
+	return JsonResponse({'error': 'método no permitido'}, status=405)
--- Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
+++ Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios_login.urls')),
-    path('registros/', include('registros.urls')), 
+    path('api/v1/', include('registros.urls')),
 ]
