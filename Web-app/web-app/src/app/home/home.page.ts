d683dd9ee5e2809d47daaa9399683ef493ab87d7
Tiziano patella <tizianopatella40@gmail.com>
2025-09-18 16:31:55 -0300

    Nueva Interfaz home Web-app
    En esta nueva interfaz se suplantaron las opciones desplegables por botones para así mejorar el apartado visual de las opciones., además se implemento la opción de al apretar en cualquiera de los botones, se despliegue todo hacia la toolbar para permitir la visualización de la interfaz visual.

--- Web-app/web-app/src/app/home/home.page.ts
+++ Web-app/web-app/src/app/home/home.page.ts
@@ -4,12 +4,25 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class HomePage {
-  botones: string[] = ['Opción 1', 'Opción 2'];
+  mostrarToolbarVisual = false;
 
-  agregarBoton() {
-    this.botones.push('Nuevo espacio');
+  opcionesMenu: string[] = [
+    'Interfaz Visual',
+    'Registros y Movimientos',
+    'Reportes',
+    'Gestión de Pañol 1',
+    'Gestión de Pañol 2',
+    'Gestión de Pañol 3'
+  ];
+
+  activarInterfazVisual() {
+    this.mostrarToolbarVisual = true;
+  }
+
+  volverAlMenuCentral() {
+    this.mostrarToolbarVisual = false;
   }
 }
