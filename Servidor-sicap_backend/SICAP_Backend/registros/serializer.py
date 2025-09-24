6cc58e0587a4c2a766af9949dbee6610eb7e3ba1
facuspagnoletta <101607658+facuspagnoletta@users.noreply.github.com>
2025-09-24 18:47:39 -0300

    Agrego el modelo para menu

--- Servidor-sicap_backend/SICAP_Backend/registros/serializer.py
+++ Servidor-sicap_backend/SICAP_Backend/registros/serializer.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Panol  # y agrega otros modelos si los usás (p.ej. RegistroTag, TagPerfil)
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Panol 
 
 class PanolSerializer(serializers.ModelSerializer):
     class Meta:
         model = Panol
         fields = ['id', 'nombre', 'icono']
+
+class PanolViewSet(ModelViewSet):
+    queryset = Panol.objects.all().order_by('nombre')
+    serializer_class = PanolSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
