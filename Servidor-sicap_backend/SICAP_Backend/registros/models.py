e36a429ed6767bdffe905c8961a1e7ec52e0c295
facuspagnoletta <facu.spagnoletta@gmail.com>
2025-09-26 14:25:03 -0300

    hora

--- Servidor-sicap_backend/SICAP_Backend/registros/models.py
+++ Servidor-sicap_backend/SICAP_Backend/registros/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class RegistroTag(models.Model):
     tag = models.CharField(max_length=100)
-    fecha_horaen  = models.DateTimeField(auto_now_add=True)
+    fecha_hora  = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.tag} - {self.fecha_hora}"
