eef58f7d19e16c3d1862f3c20014e809930d5410
facuspagnoletta <facu.spagnoletta@gmail.com>
2025-09-26 20:36:42 -0300

    agrego una url

--- Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
+++ Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 def get_csrf(request):
     return JsonResponse({"detail": "CSRF Cookie Set"})
 urlpatterns = [
+		path('',home),
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios_login.urls')),
     path('api/v1/', include('registros.urls')),    
