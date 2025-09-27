789b0dcc7363387fc34eac51464ca39a3aaa1a86
facuspagnoletta <facu.spagnoletta@gmail.com>
2025-09-26 21:34:00 -0300

    Backend funcionando de manera autónoma

--- Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
+++ Servidor-sicap_backend/SICAP_Backend/sicap_backend/urls.py
@@ -5,6 +5,9 @@ from django.http import JsonResponse
 
 def get_csrf(request):
     return JsonResponse({"detail": "CSRF Cookie Set"})
+
+def home(request):
+		return JsonResponse({"status":"ok","message":"SICAP Backend activo"})
 urlpatterns = [
 		path('',home),
     path('admin/', admin.site.urls),
