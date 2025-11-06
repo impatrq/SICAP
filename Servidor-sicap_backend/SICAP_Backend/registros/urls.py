a7be3967b97b99c7f56d23eccb4b3fb223392b7f
facuspagnoletta <facu.spagnoletta@gmail.com>
2025-11-06 09:48:40 -0800

    Agrego al Backend Nueva función para eliminar tags

--- Servidor-sicap_backend/SICAP_Backend/registros/urls.py
+++ Servidor-sicap_backend/SICAP_Backend/registros/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework import routers
-from .views import recibir_tag, listar_tags, PanolViewSet, csrf_ping, RegistroTagViewSet, editar_tag, assignments_list, assignments_auto, assignments_devolver
+from .views import recibir_tag, listar_tags, PanolViewSet, csrf_ping, RegistroTagViewSet, editar_tag, assignments_list, assignments_auto, assignments_devolver, bulk_delete_uncategorized_tags
 
 router = DefaultRouter()
 router.register(r'panoles', PanolViewSet, basename='panoles')
@@ -12,6 +12,7 @@ urlpatterns = [
     path('', include (router.urls)),
     path('register/tag/', recibir_tag, name= "recibir-tag"),
     path('register/tag/list/', listar_tags, name="listar-tags"),  
+    path('register/tag/bulk_delete/', bulk_delete_uncategorized_tags, name='registrotag-bulk-delete'),
     path('register/tag/<int:id>/editar/', editar_tag, name="editar-tag"),
     path('assignments/', assignments_list, name='assignments_list'),
     path('assignments/auto/', assignments_auto, name='assignments_auto'),
